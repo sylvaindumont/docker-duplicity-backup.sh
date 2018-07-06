525198c4aea78d4b19788d022a7dd3bfd7fdb093
ferron <ferronrsmith@gmail.com>
2018-07-06 08:13:22 -0400

    allow bucket location to be configured via envar

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 cat <<EOF > /home/duplicity/.s3cfg
 $([ "$(echo "$DEST" | cut -d'/' -f1)" == "s3:" ] && echo "host_base = $(echo "$DEST" | cut -d'/' -f3)")
 $([ "$(echo "$DEST" | cut -d'/' -f1)" == "s3:" ] && echo "host_bucket = $(echo "$DEST" | cut -d'/' -f3)")
-bucket_location = us-east-1
+bucket_location = ${BUCKET_LOCATION:-us-east-1}
 use_https = True
 access_key = ${AWS_ACCESS_KEY_ID}
 secret_key = ${AWS_SECRET_ACCESS_KEY}
